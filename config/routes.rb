06e0e62456a0eb04803c6cc1166eeb397d5f867b
Jyoti Sharma <jyoti@joshsoftware.com>
2018-09-25 13:56:39 +0530

    route for currency convertor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :currencies, only: [:index]
+  resource :currency_convertor, only: [:show], controller: :currency_convertor
 end
